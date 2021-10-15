d2e8ab507e5cfdbd88a9d8efb49e0ca8f1693839
Brian Seeders <brian.seeders@elastic.co>
2021-10-15 12:19:04 -0400

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -2,4 +2,5 @@
 
 KIBANAMACHINE_TOKEN="$(vault read -field=github_token secret/kibana-issues/dev/buildkite-github-credentials)"
 echo "$KIBANAMACHINE_TOKEN" | gh auth login --with-token
+git config --global credential.https://github.com.username brianseeders
 git clone https://github.com/elastic/kibana-ci-stats.git
