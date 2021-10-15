6630ea67d973ec1d32dbb4482b907fe28c783a19
Brian Seeders <brian.seeders@elastic.co>
2021-10-15 15:35:38 -0400

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+{
+  KIBANAMACHINE_TOKEN="$(retry 5 15 vault read -field=github_token secret/kibana-issues/dev/kibanamachine)"
+  git config --global credential.helper cache
+  echo "protocol=https
+host=github.com
+username=token
+password=$KIBANAMACHINE_TOKEN" | git credential-cache store
+}
+
 git clone https://github.com/elastic/kibana-ci-stats.git
